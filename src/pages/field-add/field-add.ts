d7e3760d7336674f2b9d1bc4cab25845270f0381
Christian Cook <christian@elixel.co.uk>
2017-03-08 16:07:41 +0000

    Add Field wireframe

--- src/pages/field-add/field-add.ts
+++ src/pages/field-add/field-add.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import { Map } from 'mapbox-gl';
 import { NavController, NavParams } from 'ionic-angular';
 
 /*
@@ -12,11 +14,29 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'field-add.html'
 })
 export class FieldAddPage {
+  map: Map<any, any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // Set public access token
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY29va2llY29va3NvbiIsImEiOiJjaXp6b3dvZnEwMDNqMnFsdTdlbmJtcHY0In0.OeHfq5_gzEIW13JzzsZJEA';
+  }
+
+  ngOnInit() {
+    // Create map instance
+    this.map = new mapboxgl.Map({
+      container: 'map-add',
+      style: 'mapbox://styles/mapbox/satellite-v9',
+      zoom: 12,
+      center: [-4.146236, 50.373528]
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FieldAddPage');
+  }
+
+  addField() {
+    this.navCtrl.pop();
   }
 
 }
