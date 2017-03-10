0fa3db6f55dc607c1beda26be3bf224268610ec2
Christian Cook <christian@elixel.co.uk>
2017-03-10 14:49:41 +0000

    added calculator wireframes

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Slides } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 
 /*
   Generated class for the Calculator page.
@@ -12,11 +13,22 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'calculator.html'
 })
 export class CalculatorPage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalculatorPage');
+  }
+
+  // Previous button handler
+  prevPressed() {
+    this.slides.slidePrev();
+  }
+
+  // Next button handler
+  nextPressed() {
+    this.slides.slideNext();
   }
 
 }
